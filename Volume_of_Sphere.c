9dda140f47c1040f9b6efecdb8ffb3bfddd33898
VelusuriSailu03 <147792494+VelusuriSailu03@users.noreply.github.com>
2023-10-14 10:05:59 +0530

    #include<stdio.h> int main() {     int r=2;     scanf("%d",&r);     float b=3.14;     scanf("%f",&b);     float v=4*b*r*r*r/3;     scanf("%f",v);     printf("%.2f",v);     return 0;
    }

---
+++ Volume_of_Sphere.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int r=2;
+    scanf("%d",&r);
+    float b=3.14;
+    scanf("%f",&b);
+    float v=4*b*r*r*r/3;
+    scanf("%f",v);
+    printf("%.2f",v);
+    return 0;
+    
+}
