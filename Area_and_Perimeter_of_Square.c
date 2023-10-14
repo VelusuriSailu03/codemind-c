1db2b2bd794bb373acf231110fbd2a398db1d22a
VelusuriSailu03 <147792494+VelusuriSailu03@users.noreply.github.com>
2023-10-14 10:01:08 +0530

    #include<stdio.h> #include<math.h> int main() {     int s=7;     scanf("%d",&s);     int area=s*s;     printf("%d",area);     int per=4*s;     printf(" %d",per);     return 0; }

---
+++ Area_and_Perimeter_of_Square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int s=7;
+    scanf("%d",&s);
+    int area=s*s;
+    printf("%d",area);
+    int per=4*s;
+    printf(" %d",per);
+    return 0;
+}
