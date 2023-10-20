324c88f3a4e3b6bbaeb7280c2063975f2a643cd4
VelusuriSailu03 <147792494+VelusuriSailu03@users.noreply.github.com>
2023-10-20 11:04:06 +0530

    #include<stdio.h> int main() {     int X,A,B;     scanf("%d%d%d",&X,&A,&B);     if(1*A+2*B>=X)     {         printf("Qualify");     }     else     {         printf("Not Qualify");     }
    }

---
+++ Contest:.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int X,A,B;
+    scanf("%d%d%d",&X,&A,&B);
+    if(1*A+2*B>=X)
+    {
+        printf("Qualify");
+    }
+    else 
+    {
+        printf("Not Qualify");
+    }
+    
+}
