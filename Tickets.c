2d835bf98baeb6a216ae5f28b9c320317badce3c
VelusuriSailu03 <147792494+VelusuriSailu03@users.noreply.github.com>
2023-11-09 11:33:29 +0530

    #include<stdio.h> int main() {     int cost;     scanf("%d",&cost);     if(cost*4<=1000)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Tickets.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int cost;
+    scanf("%d",&cost);
+    if(cost*4<=1000)
+    {
+        printf("YES");
+    }
+    else 
+    {
+        printf("NO");
+    }
+}
