02ec9ad3802f3e91ef22d4a5f7bd70a0538237cf
VelusuriSailu03 <147792494+VelusuriSailu03@users.noreply.github.com>
2023-12-16 00:57:42 -0500

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n],sum=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=1;i<n;i++)     {         if(i%2==1)         {             sum=sum+a[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<n;i++)
+    {
+        if(i%2==1)
+        {
+            sum=sum+a[i];
+        }
+    }
+    printf("%d",sum);
+}
